360cb01e04166638182eb8cdd285c43d2acf967f
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-11-25 18:46:15 -0600

    Static File Embedder and server

---
+++ client/clienthandler.go
@@ -0,0 +1,81 @@
+package clienthandler
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed static/*
+var staticFS embed.FS
+
+// AddRoutes serves the static file system for the Angular Web App.
+func AddRoutes(router gin.IRouter) {
+	embeddedBuildFolder := newStaticFileSystem()
+	fallbackFileSystem := newFallbackFileSystem(embeddedBuildFolder)
+	router.Use(static.Serve("/", embeddedBuildFolder))
+	router.Use(static.Serve("/", fallbackFileSystem))
+}
+
+// staticFileSystem serves files out of the embedded build folder
+type staticFileSystem struct {
+	http.FileSystem
+}
+
+var _ static.ServeFileSystem = (*staticFileSystem)(nil)
+
+func newStaticFileSystem() *staticFileSystem {
+	sub, err := fs.Sub(staticFS, "static")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &staticFileSystem{
+		FileSystem: http.FS(sub),
+	}
+}
+
+func (s *staticFileSystem) Exists(prefix string, path string) bool {
+	buildpath := fmt.Sprintf("static%s", path)
+
+	// support for folders
+	if strings.HasSuffix(path, "/") {
+		_, err := staticFS.ReadDir(strings.TrimSuffix(buildpath, "/"))
+		return err == nil
+	}
+
+	// support for files
+	f, err := staticFS.Open(buildpath)
+	if f != nil {
+		_ = f.Close()
+	}
+	return err == nil
+}
+
+// fallbackFileSystem wraps a staticFileSystem and always serves /index.html
+type fallbackFileSystem struct {
+	staticFileSystem *staticFileSystem
+}
+
+var _ static.ServeFileSystem = (*fallbackFileSystem)(nil)
+var _ http.FileSystem = (*fallbackFileSystem)(nil)
+
+func newFallbackFileSystem(staticFileSystem *staticFileSystem) *fallbackFileSystem {
+	return &fallbackFileSystem{
+		staticFileSystem: staticFileSystem,
+	}
+}
+
+func (f *fallbackFileSystem) Open(path string) (http.File, error) {
+	return f.staticFileSystem.Open("/index.html")
+}
+
+func (f *fallbackFileSystem) Exists(prefix string, path string) bool {
+	return true
+}
